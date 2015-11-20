a49daabe477755dad9f73a32a7f9d4a1de5bfe3e
Nolan Rigo <nolan.rigo@cpnv.ch>
2015-11-20 15:33:02 +0100

    Add 'Sources' Collections

---
+++ collections/sources.js
@@ -0,0 +1,12 @@
+Sources = new Mongo.Collection("Sources");
+Sources.allow({
+  insert: function(){
+    return true;
+  },
+  update: function(){
+    return true;
+  },
+  remove: function(){
+    return true;
+  }
+});
