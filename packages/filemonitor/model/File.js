bfbc32a4b13831585c471d5a4ca09e11f2138b6f
Nolan Rigo <nolan.rigo@gmail.com>
2015-11-30 23:19:15 +0100

    replace . by space in title

--- packages/filemonitor/model/File.js
+++ packages/filemonitor/model/File.js
@@ -4,16 +4,13 @@ var path = Npm.require('path');
 File = function(filename){
   this.path = '';
   this.filename = '';
-  this.folder = '';
-
 
   if(typeof filename == 'object'){ // From database
-    this.filename = filename.filename;
-    this.folder = filename.folder;
     this.path = filename.path;
+    this.filename = filename.name;
   }else if(typeof filename == 'string'){ // Direct pathname
     this.path = filename;
-    this.folder = path.dirname(this.path);
-    this.filename = path.basename(this.path);
+    this.filename = path.basename(this.path,path.extname(this.path));
   }
+
 };
--- packages/parse-torrent-name/parse-torrent-name.js
+++ packages/parse-torrent-name/parse-torrent-name.js
@@ -1,1 +1,9 @@
-torrentname = Npm.require('parse-torrent-name');
+var ptn = Npm.require('parse-torrent-name');
+
+torrentname = function(filename){
+  var info = ptn(filename);
+  if(info.title){
+    info.title = info.title.replace(/\./g,' ').trim();
+  }
+  return info;
+};
