e6d1bedd077f3ff069712d6d19fa1fba792250a3
Nolan Rigo <nolan.rigo@cpnv.ch>
2015-12-01 15:48:47 +0100

    Delete parse-torrent-name package, finally I use npm module in themoviedb package

--- packages/parse-torrent-name/parse-torrent-name.js
+++
@@ -1,9 +0,0 @@
-var ptn = Npm.require('parse-torrent-name');
-
-torrentname = function(filename){
-  var info = ptn(filename);
-  if(info.title){
-    info.title = info.title.replace(/\./g,' ').trim();
-  }
-  return info;
-};
