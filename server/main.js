792a662db7833fffe346dcf5eff7edfd46abcec0
Nolan Rigo <nolan.rigo@cpnv.ch>
2015-12-01 15:56:50 +0100

    Observe item added in Movies collection for start themoviedb search

--- server/main.js
+++ server/main.js
@@ -1,14 +1,30 @@
 filemonitor.on('create',function(file){
-
-  // Ajout du torrentname
-  file.torrent = torrentname(file.filename);
-
   // Création de l'entrée dans la collection
   Movies.insert(file);
 });
 filemonitor.on('delete',function(file){
   // Supression de l'entrée dans la collection
   Movies.remove(file);
+});
+
+Movies
+.find({
+  updateAt: {$gt : Date.now()}
+})
+.observe({
+  added : function(item){
+    // Il n'y a pas de themoviedb on lance la recherche
+    if(!item.themoviedb){
+      var info = themoviedb.info(item.filename);
+      if(info){
+        Movies.update(item._id,{
+          $set : {
+            themoviedb : info
+          }
+        });
+      }
+    }
+  }
 });
 
 Meteor.startup(function(){
