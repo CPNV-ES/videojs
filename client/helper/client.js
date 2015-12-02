8f87be7b8d47f263b7014a7dcbec5f447dd05d13
Nolan Rigo <nolan.rigo@gmail.com>
2015-12-03 00:22:50 +0100

    fake interface implement

--- client/helper/client.js
+++ client/helper/client.js
@@ -6,11 +6,35 @@ Template.home.helpers({
 
 Template.home.events({
   'mouseenter .movielist ul li a' : function(event, template){
+    var id = this._id;
+    var $all = $('.movielist ul li a');
+    var $this = $(event.currentTarget);
     var $movies = $('.movies');
-    var $infobar = $('.infobar');
+    var $allmovieinfo = $('.movieinfo');
+    var $movieinfo = $('.movieinfo[data-movie-id="'+id+'"]');
 
-    console.log(this);
+    $all.removeClass('active');
+    $this.addClass('active');
+
+    // Hide All, show good one
+    $allmovieinfo.hide();
+    $movieinfo.show();
+
+    // Set Background
+    var bg = $movieinfo.attr('data-movie-background');
+    $movies.css('background-image','url('+bg+')');
+  }
+});
 
 
+Template.movieinfo.helpers({
+  moviebackground : function(){
+    if(this.themoviedb){
+      return {
+        'data-movie-background' : 'http://image.tmdb.org/t/p/original'+this.themoviedb.backdrop_path
+      };
+    }else{
+      return {};
+    }
   }
 });
