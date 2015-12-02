f4f31ff1dc38a054a9932f95244ab732e96719aa
Nolan Rigo <nolan.rigo@gmail.com>
2015-12-02 20:48:35 +0100

    Work in fake interface

--- client/helper/client.js
+++ client/helper/client.js
@@ -3,3 +3,14 @@ Template.home.helpers({
     return Movies.find({},{sort: {themoviedb:{title:-1}}});
   }
 });
+
+Template.home.events({
+  'mouseenter .movielist ul li a' : function(event, template){
+    var $movies = $('.movies');
+    var $infobar = $('.infobar');
+
+    console.log(this);
+
+
+  }
+});
