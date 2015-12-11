5b1cb1ba507d155280176387f2251b375c51dbdb
Nolan Rigo <nolan.rigo@cpnv.ch>
2015-12-11 18:16:30 +0100

    Modification of fake interface

--- client/helper/home.js
+++ client/helper/home.js
@@ -1,30 +1,12 @@
 Template.home.helpers({
   movies : function(){
-    return Movies.find({},{sort: {themoviedb:{title:-1}}});
+    return Movies.find({},{$sort:{themoviedb:{title:-1}}});
   }
 });
 
-/*Template.movielist.events({
-  'mouseenter .list ul li a' : function(event){
-    console.log(this);
-    var id = this._id;
-    var $all = $('.movielist ul li a');
-    var $this = $(event.currentTarget);
-    var $movies = $('.movies');
-    var $allmovieinfo = $('.movieinfo');
-    var $movieinfo = $('.movieinfo[data-movie-id="'+id+'"]');
 
-    // Remove class and add class to current .active item
-    $all.removeClass('active');
-    $this.addClass('active');
-
-    // Hide All, show good one
-    $allmovieinfo.hide();
-    $movieinfo.show();
-
-    // Set Background
-    var bg = $movieinfo.attr('data-movie-background');
-    $movies.css('background-image','url('+bg+')');
-    console.log(this);
+Template.movieTemplate.helpers({
+  movie : function(){
+    return Movies.find(this._id);
   }
-});*/
+});
