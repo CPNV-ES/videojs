d115f0e0e34a25985cdb88c34898b29c43b82e7b
Nolan Rigo <nolan.rigo@cpnv.ch>
2015-12-14 10:51:10 +0100

    Use Jade for templating (proof of concept)

--- client/helper/home.js
+++ client/helper/home.js
@@ -1,4 +1,7 @@
 Template.home.helpers({
+  movies: function(){
+    return Movies.find({});
+  },
   openSources: function(){
     var hasSources = Sources.find({}).count() <= 0;
     var toggleSources = Session.get('toggleSources');
