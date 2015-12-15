1e942469daf79279b0cd42fae46c2710ea3777e4
Modjoo <Modjoo@users.noreply.github.com>
2015-12-15 15:32:41 +0100

    add more search

--- client/helper/header.js
+++ client/helper/header.js
@@ -1,5 +1,5 @@
 Template.header.helpers({
-    settingsFileName: function() {
+    settingsFileName: function () {
         return {
             rules: [
                 {
@@ -7,32 +7,65 @@ Template.header.helpers({
                     field: 'themoviedb.title',
                     matchAll: true,
                     template: Template.fileNameAutoComplete,
-                    selector: function (match){
+                    selector: function (match) {
                         regex = new RegExp(match, 'i');
                         return {$or: [{'filename': regex}, {'themoviedb.title': regex}]};
                     }
                 }
             ]
         };
+    },
+    uniqueGenre: function () {
+        var list = Movies.find({}).fetch();
+        var uniqueGenres = _.chain(list)
+            .pluck('themoviedb')
+                .flatten()
+            .pluck('genres')
+                .flatten()
+            .pluck('name')
+                .flatten()
+            .unique()
+                .value();
+        console.log(uniqueGenres);
+        return uniqueGenres;
     }
 });
 
 Template.header.events({
-    'keyup #querytitle': function() {
+    'keyup #querytitle': function () {
         // save the current search query in a session variable as the user types
         return Session.set('querytitle', $('#querytitle').val());
     },
-    'keyup #querycrew': function() {
+    'keyup #querycrew': function () {
         // save the current search query in a session variable as the user types
         return Session.set('querycrew', $('#querycrew').val());
     },
-    'keyup #querycast': function(){
+    'keyup #querycast': function () {
         return Session.set('querycast', $('#querycast').val())
     },
-    'click #queryclear': function(){
-        Session.set('querytitle', '');
-        Session.set('querycrew', '');
-        Session.set('querycast', '');
+    'click #queryclear': function () {
+        resetVarForm();
+        $("#queryGenre").select2('val', 'All');
         document.getElementById("form").reset();
     }
 });
+
+var resetVarForm = function (){
+    Session.set('querytitle', '');
+    Session.set('querycrew', '');
+    Session.set('querycast', '');
+    Session.set('queryGenre','');
+};
+
+
+/**
+ * TODO: refactor this crap !
+ */
+Template.header.rendered = function () {
+    $("#queryGenre").select2({
+        tags: true,
+        tokenSeparators: [',', ' ']
+    }).on('change', function(){
+        Session.set('queryGenre',$("#queryGenre").val());
+    });
+};
