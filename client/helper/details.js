669ca96486b0b7c30a597ce2f88360e9e6b70ee1
MickaelLacombe <mickael.lacombe@hotmail.fr>
2015-12-17 15:55:39 +0100

    add new css

--- client/helper/details.js
+++ client/helper/details.js
@@ -19,6 +19,7 @@ Template.details.events({
   },
   'click .unbind': function(e){
     e.preventDefault();
+    if(!confirm('Etes-vous sûr ?')) return false;
     var movie_id = this._id;
     Meteor.call('tmdbUnBind',movie_id,function(err,result){
       if(err) console.error("unbind error");
