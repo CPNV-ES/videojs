f7a2d2b5fca19889607a439fcc822059830b4d81
Nolan Rigo <nolan.rigo@gmail.com>
2015-12-17 21:28:59 +0100

    Initial problem, when we restart meteor, removed,update of old sources aren't observe. Fixe that.

--- server/file.js
+++ server/file.js
@@ -19,24 +19,30 @@ filemonitor.on('delete',function(file){
   Movies.remove(file);
 });
 
-// When source added, removed or changed -> reload filemonitor
+// When sources (just new sources) is created, start sync.
 Sources.find({
   updateAt: {
     $gt: Date.now()
   }
 }).observe({
   added: function(){ filemonitor.sources(); },
+});
+// When sources is deleted or updated, start sync.
+Sources.find({}).observe({
   removed: function(){ filemonitor.sources(); },
   changed: function(){ filemonitor.sources(); },
 });
 
-// When extension added, removed or changed -> reload filemonitor
+// When extensions (just new extensions) is created, start sync.
 Extensions.find({
   updateAt: {
     $gt: Date.now()
   }
 }).observe({
   added: function(){ filemonitor.extensions(); },
+});
+// When extensions is deleted or updated, start sync.
+Extensions.find({}).observe({
   removed: function(){ filemonitor.extensions(); },
   changed: function(){ filemonitor.extensions(); },
 });
