67aedc8a0c9de8cb9fac6e1942d267408ed34694
Nolan Rigo <nolan.rigo@gmail.com>
2015-12-18 23:18:20 +0100

    Add comment to bindTmdb/helpers

--- client/bindTmdb/helpers.js
+++ client/bindTmdb/helpers.js
@@ -1,12 +1,13 @@
 Template.tmdbBind.helpers({
-    rendered: function () {
-    },
+    // Results of search
     results: function () {
         return Session.get('tmdbBindResults');
     },
+    // Are we searching info ?
     isLoading: function () {
         return Session.get('tmdbBindLoading');
     },
+    // have we select movie for binding ?
     hasId: function () {
         return Session.get('tmdbBindId');
     },
